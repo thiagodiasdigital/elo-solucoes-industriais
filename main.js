dd28f2e6a3d74d58729db13439534e4e8fb5ac67
thiagodiasdigital <thiagodias180986@gmail.com>
2026-05-22 17:27:05 -0300

    Add partner brands carousel

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ if (menuButton && navLinks) {
     menuButton.setAttribute("aria-expanded", String(isOpen));
   });
 }
+
+
+if (typeof Swiper !== "undefined" && document.querySelector(".brand-swiper")) {
+  new Swiper(".brand-swiper", {
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+    },
+    breakpoints: {
+      0: { slidesPerView: 1, spaceBetween: 42 },
+      700: { slidesPerView: 4, spaceBetween: 42 },
+      1100: { slidesPerView: 6, spaceBetween: 42 }
+    },
+    loop: true,
+    speed: 650,
+    slidesPerView: 1,
+    spaceBetween: 42
+  });
+}
